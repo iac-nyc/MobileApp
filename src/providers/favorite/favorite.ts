4fcc6b38d4bd1e3d3487721da90f0480934a0eb5
Iftekhar Chowdhury <iftekhar_chowdhury_bd@yahoo.com>
2017-07-04 01:16:53 -0400

    FloatingActionButton favorites

---
+++ src/providers/favorite/favorite.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the FavoriteProvider provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular 2 DI.
+*/
+@Injectable()
+export class FavoriteProvider {
+    favorites: Array<any>;
+
+  constructor(public http: Http) {
+    console.log('Hello FavoriteProvider Provider');
+    this.favorites=[];
+  }
+  addFavorite(id: number): boolean{
+  this.favorites.push(id);
+  return true;
+  }
+  
+  isFavorite(id: number): boolean{
+  return this.favorites.some(el => el === id);
+  }
+
+}
