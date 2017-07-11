0dbc82bb99cf2bff8aaab5767692bafe60543317
Iftekhar Chowdhury <iftekhar_chowdhury_bd@yahoo.com>
2017-07-10 22:10:02 -0400

    Alerts, Toasts and Loading

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { IonicPage, NavController, NavParams, ItemSliding } from 'ionic-angular';
+/**import { IonicPage, NavController, NavParams, ItemSliding } from 'ionic-angular';
+*
+*
+*import { IonicPage, NavController, NavParams, ItemSliding, *ToastController } from 'ionic-angular';
+*/
+import { IonicPage, NavController, NavParams, ItemSliding, ToastController, LoadingController, AlertController } from 'ionic-angular';
+
 import { FavoriteProvider } from '../../providers/favorite/favorite';
 import { Dish } from '../../shared/dish';
 
@@ -19,10 +25,21 @@ export class FavoritesPage implements OnInit {
   favorites: Dish[];
   errMess: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,
+  /*constructor(public navCtrl: NavController, public navParams: NavParams,
     private favoriteservice: FavoriteProvider,
-    @Inject('BaseURL') private BaseURL) {
+    @Inject('BaseURL') private BaseURL,
+    public toastCtrl: ToastController) {
   }
+  **/
+  
+   constructor(public navCtrl: NavController, public navParams: NavParams,
+    private favoriteservice: FavoriteProvider,
+    @Inject('BaseURL') private BaseURL,
+    private toastCtrl: ToastController,
+    private loadingCtrl: LoadingController,
+    private alertCtrl: AlertController){
+    
+    }
 
   ngOnInit() {
     this.favoriteservice.getFavorites()
@@ -34,13 +51,58 @@ export class FavoritesPage implements OnInit {
     console.log('ionViewDidLoad FavoritesPage');
   }
 
-  deleteFavorite(item: ItemSliding, id: number) {
+ /** deleteFavorite(item: ItemSliding, id: number) {
     console.log('delete', id);
     this.favoriteservice.deleteFavorite(id)
       .subscribe(favorites => this.favorites = favorites,
         errmess => this.errMess = errmess);
     item.close();
+    
+      this.toastCtrl.create({
+      message: 'Dish ' + id + ' deleted successfully', 
+      duration: 3000}).present();
+    item.close();
 
   }
+  **/
+    deleteFavorite(item: ItemSliding, id: number) {
+    console.log('delete', id);
+
+    let alert = this.alertCtrl.create({
+      title: 'Confirm Delete',
+      message: 'Do you want to delete Dish '+ id,
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Delete cancelled');
+          }
+        },
+        {
+          text: 'Delete',
+          handler: () => {
+            let loading = this.loadingCtrl.create({
+              content: 'Deleting . . .'
+            });
+            let toast = this.toastCtrl.create({
+              message: 'Dish ' + id + ' deleted successfully', 
+              duration: 3000});
+            loading.present();
+            this.favoriteservice.deleteFavorite(id)
+              .subscribe(favorites => {this.favorites = favorites; loading.dismiss(); toast.present(); } ,
+                errmess =>{ this.errMess = errmess; loading.dismiss(); });
+          }
+        }
+      ]
+    });
+  
+    alert.present();
+
+    item.close();
+
+  }
+
+  
 
 }
